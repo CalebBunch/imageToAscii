e772e658709ba0df3799f983578605301b305fe8
CalebBunch <rcbcg123@gmail.com>
2024-06-29 16:18:58 -0400

    fixed error in load image function, colors are different now

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"log"
 	"os"
   "image/color"
-  "reflect"
 )
 
 const PATH string = "image.png"
@@ -18,11 +17,11 @@ func main() {
   fmt.Println(matrix[0][0])
   for i := 0; i < len(matrix[0]); i++ {
     for j := 0; j < len(matrix); j++ {
-      fmt.Print()
-      //fmt.Print(matrix[i][j])
-      //fmt.Println(toAscii(matrix[i][j]))
+      // fmt.Print()
+      // fmt.Print(matrix[i][j])
+      fmt.Println(toAscii(matrix[i][j]))
     }
-    //fmt.Println()
+    fmt.Println()
   }
 }
 
@@ -35,10 +34,13 @@ func readImage(path string) [][]float64 {
 	if err != nil {
 		log.Fatal(err)
 	}
-  fmt.Println(reflect.TypeOf(img))
+  
+  // fmt.Println(img)
+  // fmt.Println(img.ColorModel())
+
 
 	bounds := img.Bounds()
-  fmt.Println(bounds)
+  // fmt.Println(bounds)
   bx := int((bounds.Max.X - bounds.Min.X)/CR)
   by := int((bounds.Max.Y - bounds.Min.Y)/CR)
   
@@ -46,20 +48,35 @@ func readImage(path string) [][]float64 {
   for i := range img_matrix {
     img_matrix[i] = make([]float64, by)
   }
+  
+  // fmt.Println(bx, by)
 
-  for i := bounds.Min.X; i < ((bounds.Max.X/CR) - ((bounds.Max.X/CR) % CR)); i += CR {
-    for j := bounds.Min.Y; j < ((bounds.Max.Y/CR) - ((bounds.Max.Y/CR)%CR)); j += CR {
+  x, y := 0, 0
+  for i := bounds.Min.X; i < (bounds.Max.X - (bounds.Max.X % CR)); i += CR {
+    for j := bounds.Min.Y; j < (bounds.Max.Y - (bounds.Max.Y % CR)); j += CR {
       avg_value := 0.0
       for k := i; k < i + CR; k++ {
         for l := j; l < j + CR; l++ {
+          // fmt.Println(k, l)
+          // fmt.Println(img.At(k, l))
           avg_value += convertRGB(img.At(k, l))
           //fmt.Println(img.At(k, l))
         }
       }
-      img_matrix[i][j] = avg_value / float64(CR*CR)
+      img_matrix[x][y] = avg_value / float64(CR*CR)
       // fmt.Println(avg_value)
+      y++
 		}
+    y = 0
+    x++
 	}
+  /*
+  for _, h := range img_matrix {
+    for _, g := range h {
+      fmt.Println(g)
+    }
+  }
+  */
   return img_matrix
 }
 
