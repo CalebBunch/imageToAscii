bc7b3f0e9a4071a63102041b5d36806b1a3e1632
Luke Jennings <luke@lawn-128-61-54-30.lawn.gatech.edu>
2024-06-29 10:11:55 -0400

    added toascii function, color grabbing needs to be refactored

--- main.go
+++ main.go
@@ -6,17 +6,23 @@ import (
 	"log"
 	"os"
   "image/color"
+  "reflect"
 )
 
 const PATH string = "image.png"
+const ASCIIMAP string = ".:!i><~+_-?][}{1)(/0*#&8%@$"
+const CR int = 8
 
 func main() {
   matrix := readImage(PATH)
   fmt.Println(matrix[0][0])
-  for i := 0; i < len(matrix); i++ {
-    for j := 0; j < len(matrix[0]); j++ {
-      fmt.Println(matrix[i][j])
+  for i := 0; i < len(matrix[0]); i++ {
+    for j := 0; j < len(matrix); j++ {
+      fmt.Print()
+      //fmt.Print(matrix[i][j])
+      //fmt.Println(toAscii(matrix[i][j]))
     }
+    //fmt.Println()
   }
 }
 
@@ -29,29 +35,37 @@ func readImage(path string) [][]float64 {
 	if err != nil {
 		log.Fatal(err)
 	}
+  fmt.Println(reflect.TypeOf(img))
 
 	bounds := img.Bounds()
   fmt.Println(bounds)
-  bx := int((bounds.Max.X - bounds.Min.X)/4)
-  by := int((bounds.Max.Y - bounds.Min.Y)/4)
+  bx := int((bounds.Max.X - bounds.Min.X)/CR)
+  by := int((bounds.Max.Y - bounds.Min.Y)/CR)
   
   img_matrix := make([][]float64, bx)
   for i := range img_matrix {
     img_matrix[i] = make([]float64, by)
   }
 
-  for i := bounds.Min.X; i < ((bounds.Max.X/4) - ((bounds.Max.X/4) % 4)); i += 4 {
-    for j := bounds.Min.Y; j < ((bounds.Max.Y/4) - ((bounds.Max.Y/4)%4)); j += 4 {
+  for i := bounds.Min.X; i < ((bounds.Max.X/CR) - ((bounds.Max.X/CR) % CR)); i += CR {
+    for j := bounds.Min.Y; j < ((bounds.Max.Y/CR) - ((bounds.Max.Y/CR)%CR)); j += CR {
       avg_value := 0.0
-      for k := i; k < i + 4; k++ {
-        for l := j; l < j + 4; l++ {
-          avg_value += convertRGB(img.At(i, j))
+      for k := i; k < i + CR; k++ {
+        for l := j; l < j + CR; l++ {
+          avg_value += convertRGB(img.At(k, l))
+          //fmt.Println(img.At(k, l))
         }
       }
-      img_matrix[i][j] = avg_value / 16.0
+      img_matrix[i][j] = avg_value / float64(CR*CR)
+      // fmt.Println(avg_value)
 		}
 	}
   return img_matrix
+}
+
+func toAscii(shade float64) string {
+  index := int(shade/(255.0/float64(len(ASCIIMAP))))
+  return string(ASCIIMAP[index])
 }
 
 func convertRGB(c color.Color) float64 {
