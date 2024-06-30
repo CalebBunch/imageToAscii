41869958ee948271605f13a0228caf0e6d982447
Luke Jennings <luke@localhost.localdomain>
2024-06-29 22:51:50 -0400

    working, flags added

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"image/color"
 	"image/png"
@@ -8,26 +9,29 @@ import (
 	"os"
 )
 
-const PATH string = "image.png"
-const ASCIIMAP string = ".:!i><~+_-?][}{1)(/0*#&8%@$"
-const CR int = 8
+const ASCIIMAP string = " .:!i><~+_-?][}{1)(/0*#&8%@$"
+
+var PATH string
+var CRH int
+var CRV int
 
 func main() {
+	cx := flag.Int("cx", 20, "X Compression")
+	cy := flag.Int("cy", 30, "Y Compression")
+	path := flag.String("path", "image.png", "Path to Image")
+	flag.Parse()
+	CRH = *cx
+	CRV = *cy
+	PATH = *path
+
 	matrix := readImage(PATH)
-	/*
-  for _, row := range matrix {
-		for _, val := range row {
-		  fmt.Print(toAscii(val))
+	for i := 0; i < len(matrix[0]); i++ {
+		for j := 0; j < len(matrix); j++ {
+			fmt.Print(toAscii(matrix[j][i]))
 		}
 		fmt.Println()
 	}
-  */
-  for i := 0; i < len(matrix); i++ {
-    for j := 0; j < len(matrix[i]); j++ {
-      fmt.Print(toAscii(matrix[i][j]))
-    }
-    fmt.Println()
-  }
+
 }
 
 func readImage(path string) [][]float64 {
@@ -41,8 +45,8 @@ func readImage(path string) [][]float64 {
 	}
 
 	bounds := img.Bounds()
-	bx := int((bounds.Max.X - bounds.Min.X) / CR)
-	by := int((bounds.Max.Y - bounds.Min.Y) / CR)
+	bx := int((bounds.Max.X - bounds.Min.X) / CRH)
+	by := int((bounds.Max.Y - bounds.Min.Y) / CRV)
 
 	img_matrix := make([][]float64, bx)
 	for i := range img_matrix {
@@ -50,15 +54,15 @@ func readImage(path string) [][]float64 {
 	}
 
 	x, y := 0, 0
-	for i := bounds.Min.X; i < (bounds.Max.X - (bounds.Max.X % CR)); i += CR {
-		for j := bounds.Min.Y; j < (bounds.Max.Y - (bounds.Max.Y % CR)); j += CR {
+	for i := bounds.Min.X; i < (bounds.Max.X - (bounds.Max.X % CRH)); i += CRH {
+		for j := bounds.Min.Y; j < (bounds.Max.Y - (bounds.Max.Y % CRV)); j += CRV {
 			avg_value := 0.0
-			for k := i; k < i+CR; k++ {
-				for l := j; l < j+CR; l++ {
+			for k := i; k < i+CRH; k++ {
+				for l := j; l < j+CRV; l++ {
 					avg_value += convertRGB(img.At(k, l))
 				}
 			}
-			img_matrix[x][y] = avg_value / float64(CR*CR)
+			img_matrix[x][y] = avg_value / float64(CRH*CRV)
 			y++
 		}
 		y = 0
@@ -73,7 +77,6 @@ func toAscii(shade float64) string {
 }
 
 func convertRGB(c color.Color) float64 {
-	// [0, 65535]
 	r, g, b, _ := c.RGBA()
 	shade := 0.299*float64(r) + 0.587*float64(g) + 0.114*float64(b)
 	shade *= (255.0 / 65535.0)
