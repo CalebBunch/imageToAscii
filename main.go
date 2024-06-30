b04b85ea1f1690f30f47af2ea232abd8f2c1ae41
CalebBunch <rcbcg123@gmail.com>
2024-06-29 20:56:37 -0400

    working but image is flipped wrong direction

--- main.go
+++ main.go
@@ -14,12 +14,9 @@ const CR int = 8
 
 func main() {
   matrix := readImage(PATH)
-  fmt.Println(matrix[0][0])
-  for i := 0; i < len(matrix[0]); i++ {
-    for j := 0; j < len(matrix); j++ {
-      // fmt.Print()
-      // fmt.Print(matrix[i][j])
-      fmt.Println(toAscii(matrix[i][j]))
+  for _, row := range matrix {
+    for _, val := range row {
+      fmt.Print(toAscii(val))
     }
     fmt.Println()
   }
@@ -27,20 +24,18 @@ func main() {
 
 func readImage(path string) [][]float64 {
 	file, err := os.Open(path)
-	if err != nil {
+	
+  if err != nil {
 		log.Fatal(err)
 	}
-	img, err := png.Decode(file)
-	if err != nil {
+	
+  img, err := png.Decode(file)
+	
+  if err != nil {
 		log.Fatal(err)
 	}
   
-  // fmt.Println(img)
-  // fmt.Println(img.ColorModel())
-
-
 	bounds := img.Bounds()
-  // fmt.Println(bounds)
   bx := int((bounds.Max.X - bounds.Min.X)/CR)
   by := int((bounds.Max.Y - bounds.Min.Y)/CR)
   
@@ -49,34 +44,21 @@ func readImage(path string) [][]float64 {
     img_matrix[i] = make([]float64, by)
   }
   
-  // fmt.Println(bx, by)
-
   x, y := 0, 0
   for i := bounds.Min.X; i < (bounds.Max.X - (bounds.Max.X % CR)); i += CR {
     for j := bounds.Min.Y; j < (bounds.Max.Y - (bounds.Max.Y % CR)); j += CR {
       avg_value := 0.0
       for k := i; k < i + CR; k++ {
         for l := j; l < j + CR; l++ {
-          // fmt.Println(k, l)
-          // fmt.Println(img.At(k, l))
           avg_value += convertRGB(img.At(k, l))
-          //fmt.Println(img.At(k, l))
         }
       }
       img_matrix[x][y] = avg_value / float64(CR*CR)
-      // fmt.Println(avg_value)
       y++
 		}
     y = 0
     x++
 	}
-  /*
-  for _, h := range img_matrix {
-    for _, g := range h {
-      fmt.Println(g)
-    }
-  }
-  */
   return img_matrix
 }
 
